32ebdc2b55877db9098b76c985a393e5dd266965
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-09 23:24:45 +0100

    fix gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -20,7 +20,7 @@ import img15 from "../../assets/salon/test.png";
 const Gallery = () => {
   const imgs = [
     img1, img2, img3, img4, img5, img6, img7,
-    img8, img9, img10, img11, img12, img13, img14, img15
+    img8, img9, img10, img12, img13, img14, img15
   ];
 
   return (
